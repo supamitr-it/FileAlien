27636794cea8e31e178a51833931524ccbfd2f04
SPM-IT <p.sujitta@supamitrhospital.com>
2025-12-31 14:55:02 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>My File Server</title>
+</head>
+<body>
+  <h1>My File Server</h1>
+  <ul>
+    <li><a href="files/test.pdf">test.pdf</a></li>
+    <li><a href="files/data.json">data.json</a></li>
+  </ul>
+</body>
+</html>
